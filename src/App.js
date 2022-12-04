b2399e50a04e836540b4ff89cd14eff1af0b6558
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-12-04 18:26:33 -0500

    clarafai api added, response is getting an error, maybe due to the updated api instructions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+//our face detection API
+const app = new Clarifai.App({ apiKey: "7523494fd75c4a70b1f43a9bd6ccc4b1" });
 
 class App extends Component {
   constructor() {
@@ -21,6 +25,19 @@ class App extends Component {
   //function to detect image after button pressed
   onButtonSubmit = () => {
     console.log("button pressed");
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
   };
 
   render() {
