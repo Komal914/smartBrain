c744c895a6377d059642961171dba4a2a17249b3
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-12-29 20:01:30 -0500

    removing helper function from cardlist

--- src/components/Cardlist/Cardlist.js
+++ src/components/Cardlist/Cardlist.js
@@ -2,11 +2,6 @@ import React from "react";
 import Card from "../Card/Card";
 
 const CardList = ({ descriptions }) => {
-  const list = () => {
-    console.log("hello");
-    descriptions.map((card) => console.log(card.name));
-  };
-  list();
   return (
     <div className="card-list">
       {descriptions.map((card) => (
